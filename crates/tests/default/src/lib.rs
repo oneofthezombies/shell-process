e501d24af9c81c4511e7ca8368a2beee444d30a4
oneofthezombies <hunhoekim@gmail.com>
2024-02-07 21:29:21 +0900

    test: update unix

--- crates/tests/default/src/lib.rs
+++ crates/tests/default/src/lib.rs
@@ -3,15 +3,6 @@ mod tests {
     use sheller::Sheller;
     use std::{env, ffi::OsStr, process};
 
-    #[cfg(unix)]
-    fn get_program() -> String {
-        if let Some(_) = env::var("GITHUB_ACTIONS").ok() {
-            "/bin/bash".to_string()
-        } else {
-            env::var("SHELL").unwrap()
-        }
-    }
-
     #[test]
     #[cfg(windows)]
     fn env_shell_windows() {
@@ -36,14 +27,14 @@ mod tests {
     #[test]
     #[cfg(unix)]
     fn default_unix() {
+        use std::path::Path;
+
         let sheller = Sheller::new();
         let command = sheller.build();
-        let mut expected_command = process::Command::new(get_program());
-        expected_command.arg("-c");
-        assert_eq!(command.get_program(), expected_command.get_program());
-        assert_eq!(
-            command.get_args().collect::<Vec<&OsStr>>(),
-            expected_command.get_args().collect::<Vec<&OsStr>>()
-        );
+        let program = command.get_program().to_str().unwrap();
+        let file_name = Path::new(program).file_name().unwrap().to_str().unwrap();
+        assert!(file_name.ends_with("sh"));
+        let args = command.get_args().collect::<Vec<&OsStr>>();
+        assert_eq!(args, vec!["-c"]);
     }
 }
