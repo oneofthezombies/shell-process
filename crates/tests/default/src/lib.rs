55849fb11d5c8b955e7ede1fce84469c42befb2a
oneofthezombies <hunhoekim@gmail.com>
2024-02-07 21:30:05 +0900

    test: fix clippy

--- crates/tests/default/src/lib.rs
+++ crates/tests/default/src/lib.rs
@@ -1,11 +1,12 @@
 #[cfg(test)]
 mod tests {
     use sheller::Sheller;
-    use std::{env, ffi::OsStr, process};
 
     #[test]
     #[cfg(windows)]
     fn env_shell_windows() {
+        use std::{env, ffi::OsStr, process};
+
         let default_shell = env::var("COMSPEC").unwrap();
         assert_eq!(default_shell, "C:\\WINDOWS\\system32\\cmd.exe");
     }
@@ -13,6 +14,8 @@ mod tests {
     #[test]
     #[cfg(windows)]
     fn default_windows() {
+        use std::{env, ffi::OsStr, process};
+
         let sheller = Sheller::new();
         let command = sheller.build();
         let mut expected_command = process::Command::new(env::var("COMSPEC").unwrap());
@@ -27,7 +30,7 @@ mod tests {
     #[test]
     #[cfg(unix)]
     fn default_unix() {
-        use std::path::Path;
+        use std::{ffi::OsStr, path::Path};
 
         let sheller = Sheller::new();
         let command = sheller.build();
