7cc3385ebd56fbade6943791b06500e9b40bf51e
oneofthezombies <hunhoekim@gmail.com>
2024-02-07 22:10:14 +0900

    test: fix build error

--- crates/tests/default/src/lib.rs
+++ crates/tests/default/src/lib.rs
@@ -6,13 +6,13 @@ mod tests {
     #[test]
     #[cfg(windows)]
     fn default_windows() {
-        let sheller = Sheller::new();
+        let sheller = Sheller::new("echo hello");
         let command = sheller.build();
         let program = command.get_program().to_str().unwrap();
         let file_name = Path::new(program).file_name().unwrap().to_str().unwrap();
         assert_eq!(file_name, "cmd.exe");
         let args = command.get_args().collect::<Vec<&OsStr>>();
-        assert_eq!(args, vec!["/D", "/S", "/C"]);
+        assert_eq!(args, vec!["/D", "/S", "/C", "echo hello"]);
     }
 
     #[test]
