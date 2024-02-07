3e515a4a92bc4825636de94071eb365380a697be
oneofthezombies <hunhoekim@gmail.com>
2024-02-07 21:16:26 +0900

    test: fix default shell

--- crates/tests/default/src/lib.rs
+++ crates/tests/default/src/lib.rs
@@ -1,15 +1,21 @@
 #[cfg(test)]
 mod tests {
     use sheller::Sheller;
-    use std::ffi::OsStr;
-    use std::process;
+    use std::{env, ffi::OsStr, process};
+
+    #[test]
+    #[cfg(windows)]
+    fn env_shell_windows() {
+        let default_shell = env::var("COMSPEC").unwrap();
+        assert_eq!(default_shell, "C:\\WINDOWS\\system32\\cmd.exe");
+    }
 
     #[test]
     #[cfg(windows)]
     fn default_windows() {
         let sheller = Sheller::new();
         let command = sheller.build();
-        let mut expected_command = process::Command::new("C:\\WINDOWS\\system32\\cmd.exe");
+        let mut expected_command = process::Command::new(env::var("COMSPEC").unwrap());
         expected_command.args(&["/D", "/S", "/C"]);
         assert_eq!(command.get_program(), expected_command.get_program());
         assert_eq!(
@@ -19,11 +25,29 @@ mod tests {
     }
 
     #[test]
+    #[cfg(target_os = "linux")]
+    fn env_shell_linux() {
+        let default_shell = env::var("SHELL").unwrap();
+        assert_eq!(default_shell, "/bin/sh");
+    }
+
+    #[test]
+    #[cfg(target_os = "macos")]
+    fn env_shell_macos() {
+        let default_shell = env::var("SHELL").unwrap();
+        let expected_shell = match env::var("GITHUB_ACTIONS") {
+            Ok(_) => "/bin/bash",
+            Err(_) => "/bin/zsh",
+        };
+        assert_eq!(default_shell, expected_shell);
+    }
+
+    #[test]
     #[cfg(unix)]
     fn default_unix() {
         let sheller = Sheller::new();
         let command = sheller.build();
-        let mut expected_command = process::Command::new("/bin/sh");
+        let mut expected_command = process::Command::new(env::var("SHELL").unwrap());
         expected_command.arg("-c");
         assert_eq!(command.get_program(), expected_command.get_program());
         assert_eq!(
