aa66657dd7baa385e8fb30cf140042f83b939020
oneofthezombies <hunhoekim@gmail.com>
2024-02-09 20:21:53 +0900

    build: add github action init

--- crates/tools/dev/src/main.rs
+++ crates/tools/dev/src/main.rs
@@ -1,6 +1,6 @@
 use clap::{Parser, Subcommand};
 use sheller::Sheller;
-use std::panic;
+use std::{env, fs, io, panic, path::Path};
 
 #[derive(Parser)]
 #[command(arg_required_else_help = true)]
@@ -53,7 +53,29 @@ fn pre_push() {
     test(None);
 }
 
+fn remove_file_force<P: AsRef<Path>>(path: P) {
+    match fs::remove_file(&path) {
+        Ok(_) => println!("File deleted successfully."),
+        Err(e) if e.kind() == io::ErrorKind::NotFound => {
+            println!("File not found, but it's ok.");
+        }
+        Err(e) => println!("Error deleting file: {}", e),
+    }
+}
+
 fn init(target: Option<String>) {
+    if env::var("GITHUB_ACTIONS").is_ok() {
+        let home = if cfg!(windows) {
+            env::var("USERPROFILE").unwrap()
+        } else {
+            env::var("HOME").unwrap()
+        };
+        remove_file_force(format!("{home}/.cargo/bin/rust-analyzer.exe"));
+        remove_file_force(format!("{home}/.cargo/bin/rustfmt.exe"));
+        remove_file_force(format!("{home}/.cargo/bin/cargo-fmt.exe"));
+        Sheller::new("rustup self update").run();
+    }
+
     Sheller::new("rustup install nightly").run();
     Sheller::new("rustup component add rustfmt --toolchain nightly").run();
     Sheller::new("rustup override set nightly").run();
